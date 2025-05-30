66d507b2fc9f04d565fcb641ca4c0fa1487e8db0
Matthew Sainsbury <matthew@sainsbury.io>
2025-05-29 17:52:16 -0700

    working keyboard with cursor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::sync::Mutex;
 
 use embedded_graphics::draw_target::DrawTarget;
-use embedded_graphics::{
-    pixelcolor::Rgb565,
-    prelude::RgbColor,
-};
+use embedded_graphics::mono_font::ascii::FONT_10X20;
+use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::prelude::{Dimensions, Point, Size, WebColors};
+use embedded_graphics::primitives::{PrimitiveStyle, Rectangle, StyledDrawable};
+use embedded_graphics::text::Text;
+use embedded_graphics::Drawable;
+use embedded_graphics::{pixelcolor::Rgb565, prelude::RgbColor};
 use embedded_hal::spi::MODE_0;
 use esp_idf_svc::hal::i2c::{I2cConfig, I2cDriver};
 use esp_idf_svc::hal::units::Hertz;
@@ -13,7 +16,8 @@ use esp_idf_svc::hal::{
     gpio::PinDriver,
     prelude::Peripherals,
     spi::{
-        config::{Config, DriverConfig}, SpiSingleDeviceDriver,
+        config::{Config, DriverConfig},
+        SpiSingleDeviceDriver,
     },
 };
 use mipidsi::{
@@ -71,20 +75,21 @@ fn main() {
         DISPLAY_BUFFER.init([0_u8; 1024]),
     );
     println!("made it here");
+    set_brightness(15, &mut display_bl, delay);
 
     let mut display = Builder::new(ST7789, di)
-        .display_size(W as u16, H as u16)
+        .display_size(W, H)
         .invert_colors(ColorInversion::Inverted)
         .orientation(Orientation::new().rotate(mipidsi::options::Rotation::Deg90))
         .init(&mut delay)
         .unwrap();
 
-    display.clear(Rgb565::RED).unwrap();
-    set_brightness(15, display_bl, delay);
+    display.clear(Rgb565::BLACK).unwrap();
+    set_brightness(13, &mut display_bl, delay);
 
     let kb_addr = 0x55;
-    let kb_brightness_cmd = 0x01;
-    let kb_alt_b_brightness_cmd = 0x02;
+    let _kb_brightness_cmd = 0x01;
+    let _kb_alt_b_brightness_cmd = 0x02;
 
     let i2c_sda = peripherals.pins.gpio18;
     let i2c_scl = peripherals.pins.gpio8;
@@ -94,14 +99,63 @@ fn main() {
 
     let mut buf: [u8; 1] = [0];
 
-    
-    
+    let mut style = PrimitiveStyle::new();
+    style.fill_color = Some(Rgb565::CSS_DARK_GRAY);
+    let mut erase_style = style;
+    erase_style.fill_color = Some(Rgb565::BLACK);
+    let text_style = MonoTextStyle::new(&FONT_10X20, Rgb565::WHITE);
+
     println!("waiting for data");
+    let mut cursor = Point::new(30, 30);
+
+    let cursor_tl = |c: &Point| *c - Point::new(0, 15);
+
+    let mut message = String::new();
+    let mut cursor_rect = Rectangle::new(cursor_tl(&cursor), Size::new(2, 20));
+    cursor_rect.draw_styled(&style, &mut display).unwrap();
     loop {
-        if i2c.read(kb_addr, &mut buf, 100000).is_ok() && buf[0] > 0{
-            print!("{}", buf[0] as char);
+        if i2c.read(kb_addr, &mut buf, 100000).is_ok() && buf[0] > 0 {
+            cursor_rect.draw_styled(&erase_style, &mut display).unwrap();
+            let c = buf[0] as char;
+            match c {
+                '\x08' => {
+                    if let Some(c) = message.chars().last() {
+                        let c_str = c.to_string();
+                        if c == '\n' {
+                            message.pop();
+                            let last_line = message.lines().last().unwrap_or("");
+                            let c_txt_bounds = Text::new(last_line, Point::default(), text_style)
+                                .bounding_box()
+                                .size;
+                            cursor.x = (c_txt_bounds.width + 30) as i32;
+                            cursor.y -= 20;
+                        } else {
+                            let mut c_txt = Text::new(&c_str, Point::default(), text_style);
+                            cursor.x -= c_txt.bounding_box().size.width as i32; // no way a character is going to be 2M pixels wide
+                            c_txt.position = cursor;
+                            c_txt.character_style.text_color = Some(Rgb565::BLACK);
+                            c_txt.draw(&mut display).unwrap();
+                            message.pop();
+                        }
+                    }
+                }
+                '\x0d' => {
+                    println!("newline");
+                    message.push('\n');
+                    cursor.y += 20;
+                    cursor.x = 30;
+                }
+                _ => {
+                    message.push(c);
+                    cursor = Text::new(&c.to_string(), cursor, text_style)
+                        .draw(&mut display)
+                        .unwrap();
+                }
+            }
+            cursor_rect.top_left = cursor_tl(&cursor);
+            cursor_rect.draw_styled(&style, &mut display).unwrap();
         }
-        
+
         delay.delay_ms(100);
     }
 }
@@ -111,7 +165,7 @@ static DISPLAY_LEVEL: Mutex<u8> = Mutex::new(16);
 
 /// 0: display off
 /// 16: maximum brightness
-fn set_brightness(level: u8, mut bl: impl embedded_hal::digital::OutputPin, delay: Delay) {
+fn set_brightness(level: u8, bl: &mut impl embedded_hal::digital::OutputPin, delay: Delay) {
     assert!(level < 17);
 
     let mut current_level = DISPLAY_LEVEL.lock().unwrap();
